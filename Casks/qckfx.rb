f1d0027495338a6ad49d243a45684c093fcaf06f
Chris Wood <christopherhwood@gmail.com>
2026-01-14 15:24:03 -0500

    Bump to v0.2.15

--- Casks/qckfx.rb
+++ Casks/qckfx.rb
@@ -1,6 +1,6 @@
 cask "qckfx" do
-  version "0.2.14"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  version "0.2.15"
+  sha256 "f1764dd6e0796338053f28eced7c0a59f0f2d37c235cabf9c65b36e050a1b721"
 
   url "https://github.com/qckfx/qckfx/releases/download/v#{version}/qckfx-#{version}.dmg"
   name "qckfx"
